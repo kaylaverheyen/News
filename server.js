3c548b5e2db6569d663cdf1815e7884d4613f2e6
kaylaverheyen <kaylaverheyen@icloud.com>
2019-02-05 11:51:26 -0600

    new heroku app

--- server.js
+++ server.js
@@ -24,22 +24,22 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapeDB";
+// var MONGODB_URI = ;
 
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect( MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds221155.mlab.com:21155/heroku_2fvm3cf1", {
 
-  useMongoClient: true
+    useMongoClient: true
 
 });
 
 var db = mongoose.connection;
 
-db.on('error', function(err){
-    console.log("DB error: "+err);
+db.on('error', function (err) {
+    console.log("DB error: " + err);
 })
 
 // mongoose.connect(
